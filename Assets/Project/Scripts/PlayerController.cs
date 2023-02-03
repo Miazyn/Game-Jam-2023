2440a4907061f4afb8670f5c5347c431f674ac67
Miazyn <mikeymilla@gmail.com>
2023-02-04 00:26:07 +0100

    Added: Player Movement

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -4,5 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public InputControls controls { get; private set; }
+
+    private Vector2 moveDirection;
+
+    [SerializeField] float movementSpeed = 5;
+
+    private void Awake()
+    {
+        controls = new InputControls();
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = controls.Player.Move.ReadValue<Vector2>();
+
+        transform.position += new Vector3(moveDirection.x, moveDirection.y, 0) * movementSpeed * Time.deltaTime;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
 }
